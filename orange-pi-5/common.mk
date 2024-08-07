840c0815b6483fa27051801d9e798e239625fc2b
FooFooDamon <udc577@126.com>
2024-08-07 16:31:06 +0800

    orange-pi-5/common.mk: Add targets: update_fdt_dir and fix_clangd_db

--- orange-pi-5/common.mk
+++ orange-pi-5/common.mk
@@ -12,3 +12,18 @@ CUSTOM_FILES += arch/${ARCH}/boot/dts/rockchip/Makefile \
     arch/${ARCH}/boot/dts/rockchip/overlay/rk3588-ov13855-c2.dts \
     drivers/media/i2c/ov13855.c
 
+USER_HELP_PRINTS ?= ${DEFAULT_USER_HELP_PRINTS} \
+    echo "  * ${MAKE} update_fdt_dir"; \
+    echo "  * ${MAKE} fix_clangd_db";
+
+.PHONY: update_fdt_dir fix_clangd_db
+
+update_fdt_dir:
+	krelease=$$(${MAKE} kernelrelease -C ${SRC_ROOT_DIR} ${MAKE_ARGS} -s | grep "^[0-9]\+"); \
+	sed -i "s/\(fdt_dir=\).*/\1dtbs\/$${krelease}/" ${INSTALL_DIR}/orangepiEnv.txt
+
+dtbs_install: update_fdt_dir
+
+fix_clangd_db:
+	sed -i -e '/"-f/d' -e '/"-mabi=lp64"/d' ${SRC_ROOT_DIR}/compile_commands.json
+
